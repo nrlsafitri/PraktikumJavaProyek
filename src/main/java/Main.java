bff02528cdbb36a9e4d10e2ede38fc273834ecb4
nurulsafitri <nurulsafit@gmail.com>
2022-06-29 15:16:31 +0700

    Membuat KabupatenViewFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import frame.KabupatenViewFrame;
 import helpers.Koneksi;
 public class Main {
     public static void main(String[] args){
-        Koneksi.getConnection();
+       // Koneksi.getConnection();
+        KabupatenViewFrame viewFrame= new KabupatenViewFrame();
+        viewFrame.setVisible(true);
     }
 }
